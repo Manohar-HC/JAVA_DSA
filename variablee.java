1a44cfc3867689d1ea234f988486fac5e8ad2faf
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2026-02-26 09:48:33 +0530

    commint

---
+++ variablee.java
@@ -0,0 +1,19 @@
+public class variablee{
+    public static void main(String[] args) {
+        
+        //print your name age height and do u like coding
+
+        String name= "manu";
+        int age= 23;
+       double height= 5.8;
+       boolean dolikecoding = true;
+
+       //print the above values
+
+       System.out.println("name: "+name);
+       System.out.println("your age: "+age);
+       System.out.println("your height: "+height);
+       System.out.println("u like coding: "+dolikecoding);
+    }
+
+}
