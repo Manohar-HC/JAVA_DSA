bd9597b80a6f5aa889d9efe2f497c1c3f1f7fc48
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2026-02-26 11:28:25 +0530

    commit

---
+++ VariableDeepPractice.java
@@ -0,0 +1,128 @@
+public class VariableDeepPractice{
+
+public static void main(String[] args) {
+    
+
+
+         // declaration  int age; 
+     // initialization  age = 23;
+      // int weight = 70;  // declaration + initialization
+
+
+      /*int age = 23;
+double height = 5.8;
+float salary = 25000.50f;
+char grade = 'A';
+boolean isFit = true;
+String name = "Manohar";
+
+
+Change values:
+
+age = age + 5;
+weight = weight - 5;
+
+
+
+
+
+Step 4  Mini Challenge
+
+Make program that prints:
+
+Name
+Age now
+Age after 10 years
+Height
+Fitness status
+
+All using variables.
+
+No hardcoding inside print.
+
+Example ❌ Wrong:
+System.out.println("Age after 10 years: 33");
+
+
+
+
+*/
+
+
+
+        String name= "manu";
+        int age= 23;
+        int feautureage =age+10;
+        
+       double height= 5.8;
+       boolean dolikecoding = true;
+
+       //print the above values
+
+       System.out.println("name: "+name);
+   
+
+System.out.println("Age after 10 years: " + (age + 10));// this i also another method to change value 
+
+
+       System.out.println("your age: "+age);
+       System.out.println("feautureage: "+feautureage);
+       System.out.println("your height: "+height);
+       System.out.println("u like coding: "+dolikecoding);
+
+
+
+ /*Name: Manohar
+Current Age: 23
+Age after 5 years: 28
+Current Weight: 70
+After cutting 8kg: 62
+After bulking 5kg: 75
+Goal: Lean Muscle*/
+
+
+
+
+String namee= "manohar";
+int agee=24;
+int ageafter5year = agee+4;
+double currentweight =70;
+double aftercutting8kg = currentweight-8;
+double bulking5kg= aftercutting8kg+5;
+String goal="lean muscule";
+
+System.out.println(namee);
+System.out.println(agee);
+System.out.println( ageafter5year);
+System.out.println(currentweight);
+System.out.println(aftercutting8kg);
+System.out.println(goal);
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
