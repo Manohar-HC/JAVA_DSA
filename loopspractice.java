dd427eed0a131e3d4b173209deebf9cf4fd99018
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2026-03-01 12:18:06 +0530

    commit

---
+++ loopspractice.java
@@ -0,0 +1,32 @@
+public class loopspractice{
+
+    public static void main(String[] args) {
+        
+        // for ( int i=1; i<=10; i++)
+        // {
+        //     System.out.println(i);
+        // }
+
+
+        // for (int i=10; i>=1; i--){
+
+        //     System.out.println(i);
+        // }
+
+
+        // for (int i=2; i<=20; i+=2){
+        //     System.out.println(i);
+        // }
+
+
+        // for (int i=1; i<=10; i++){
+        //     System.out.println(i*5);
+        // }
+
+int i=1;
+while(i<=5){
+    System.out.println(i);
+    i++;
+}
+    }
+}
