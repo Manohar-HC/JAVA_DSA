723fd772a6f855d5d07d0a3e5efc639d06d37ed1
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2026-03-04 11:36:50 +0530

    commit

---
+++ arraypractice.java
@@ -0,0 +1,176 @@
+public class arraypractice{
+//     public static void main(String[] args) {
+//         int[] arr = {10, 20, 30, 40, 50};
+// for(int i=0; i< arr.length; i++){
+//     System.out.println(arr[i]);
+// }
+
+//     }
+
+
+
+
+
+
+// public static void main(String[] args) {
+    
+//     int [] arr={10,20,30,40,50};
+  
+//         System.out.println(arr[0]);
+//         System.out.println(arr[arr.length -1]);
+
+
+
+
+
+// public static void main(String[] args) {
+    
+
+//     int[] arr={ 10, 20, 30, 40, 50};
+//     int sum = 0;
+//    for(int i = 0; i < arr.length; i++){
+//         sum = sum + arr[i];
+//     }
+//           System.out.println(sum);
+// }
+
+
+
+
+
+// public static void main(String[] args) {
+    
+
+
+// int[] arr = {3, 5, 7};
+
+// int sum = 0;
+
+// for(int i = 0; i < arr.length; i++){
+//     sum = sum + arr[i];
+//     }
+//     System.out.println(sum);
+// }
+
+
+
+
+
+
+
+
+// public static void main(String[] args) {
+
+
+// int[] arr = {4, 2, 8};
+
+// for(int i = 0; i < arr.length; i++){
+//     System.out.println(arr[i] + 1);
+// }
+
+
+// }
+
+
+
+
+
+// public static void main(String[] args) {
+//    int[] arr = {8, 3, 15, 6, 2};
+
+// int max = arr[0];
+
+// for(int i = 1; i < arr.length; i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//     }
+// }
+
+// System.out.println(max);
+// }
+
+
+
+// public static void main(String[] args) {
+    
+
+
+// int[] arr = {9, 4, 7, 2, 5};
+
+// int min = arr[0];
+
+// for(int i = 1; i < arr.length; i++){
+//     if(arr[i] < min){
+//         min = arr[i];
+//     }
+// }
+
+// System.out.println(min);
+
+
+// }
+
+
+
+
+//IMPORTTANT for(int i = 0; i < arr.length; i++)Print array
+//for(int i = 0; i < arr.length; i++)
+//sum += arr[i];  Sum of array
+// if(arr[i] > max)  Find maximum
+//if(arr[i] < min) Find minimum
+
+
+
+
+// public static void main(String[] args) {
+// int[] arr = {4, 1, 3};
+
+// int sum = 0;
+
+// for(int i = 0; i < arr.length; i++){
+//     sum = sum + arr[i];
+// }
+
+// System.out.println(sum);
+// }
+
+
+
+
+/*% 2 == 0 → even number
+% 2 != 0 → odd number*/
+
+
+    // public static void main(String[] args) {
+
+    //     int[] arr = {4, 7, 2, 9, 6};
+
+    //     int count = 0;
+
+    //     for(int i = 0; i < arr.length; i++){
+    //         if(arr[i] % 2 == 0){
+    //             count++;
+    //         }
+    //     }
+
+    //     System.out.println(count);
+
+    // }
+
+
+ public static void main(String[] args) {
+    int[] arr = {2, 5, 8};
+
+for(int i = 0; i < arr.length; i++){
+    System.out.println(arr[i] * arr[i]);
+}
+}
+}
+
+
+
+
+
+
+
+
